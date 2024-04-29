37b3e6c40beaeebe709bfdde3fff49881dd14cae
Rahul Anand <duffer.rdx@gmail.com>
2024-04-30 00:11:45 +0530

    atomic basic_

---
+++ atomic-design/App.js
@@ -0,0 +1,34 @@
+/*
+
+// Atomic Design
+
+* * It is a web design methodology which helps us in streamline our workflow and improve the consistency of our design.
+* * It breaks down web design into smaller reusable components called atoms.
+ * * these atoms then combine to form molecules, organisms, templates and pages.
+ *
+ * Atoms :- Button, Form Fields, Icons, Typography
+ *
+ * Molecules :- Search Bar, Navigation Bar, Listing, BreadCrumbs
+ *
+ * Organisms :- Headers, Photos, Product List
+ *
+ * Template :- GriD layout, Structure and wireframes
+ *
+ * Pages :- Home Pages ,Landing Pages, Product Pages, Reset Password Pages,
+
+
+ * * Folder Structure
+ *  src - components
+                    -atom
+                        -Button
+                        -Input
+                        -select
+                        -Labels
+                        -Divider
+                    -molecules
+                    -organisms
+                    -templates
+                    -pages
+
+
+*/
